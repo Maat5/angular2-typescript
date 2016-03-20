c15b121463fe6322f654442e2a3fd35ba5c2182e
Manuel Abrante <mabrante@socialh4ck.com>
2016-03-20 17:43:04 -0430

    create favorite component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,17 @@
 import {Component} from 'angular2/core'
 import {CoursesComponent} from './courses.component'
 import {AuthorsComponent} from './authors.component'
+import {FavoriteComponent} from './favorite.component'
+
 
 @Component({
   selector: 'app',
   template: `
     <h1> First Angular 2 with typescript app </h1>
+    <favorite></favorite>
     <courses></courses>
     <authors></authors>`,
-  directives: [CoursesComponent, AuthorsComponent]
+  directives: [CoursesComponent, AuthorsComponent, FavoriteComponent]
 })
 
 export class AppComponent {}
