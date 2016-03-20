0515bdf7cf1220e48bd9f5e85dc27dbb7b6ef63d
Manuel Abrante <mabrante@socialh4ck.com>
2016-03-20 16:08:49 -0430

    created basic services

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,14 @@
 import {Component} from 'angular2/core'
+import {CoursesComponent} from './courses.component'
+import {AuthorsComponent} from './authors.component'
 
 @Component({
   selector: 'app',
-  template: '<h1> First Angular 2 with typescript app </h1>'
+  template: `
+    <h1> First Angular 2 with typescript app </h1>
+    <courses></courses>
+    <authors></authors>`,
+  directives: [CoursesComponent, AuthorsComponent]
 })
 
 export class AppComponent {}
