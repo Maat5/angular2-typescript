2aaa94335dddfbc27e9fc00444ea4a5c15bc0daa
Manuel Abrante <mabrante@socialh4ck.com>
2016-03-20 19:51:57 -0430

    create voter component and added some style

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,12 @@
 import {Component} from 'angular2/core'
-//import {FavoriteComponent} from './favorite.component'
+import {FavoriteComponent} from './favorite.component'
 import {LikeComponent} from './like.component'
-
+import {VoterComponent} from './voter.component'
 
 @Component({
   selector: 'app',
-  template: `
-    <likes [like]="post.liked" [count]="post.count" ></likes>`,
-  directives: [LikeComponent]
+  templateUrl: 'app/app.template.html',
+  directives: [FavoriteComponent, LikeComponent, VoterComponent]
 })
 
 export class AppComponent {
@@ -15,7 +14,15 @@ export class AppComponent {
   post = {
     liked: false,
     count: 10
-  }
+  };
 
+  voter = {
+    count: 10,
+    userVote: 0
+  };
+
+  onVote($event){
+    console.log('event', $event);
+  }
 
 }
---
+++ app/voter.component.ts
@@ -0,0 +1,57 @@
+import {Component, EventEmitter} from 'angular2/core'
+
+@Component({
+  selector: 'voter',
+  templateUrl: 'app/voter.template.html',
+  styles: [`
+
+    .voter{
+      width: 20px;
+    }
+
+    .arrows{
+      cursor: pointer;
+    }
+
+    .hightlight {
+      color: yellow;
+    }
+
+  `],
+  inputs: ['totalPoints', 'userVote'],
+  outputs: []
+})
+
+export class VoterComponent{
+  totalPoints = 0;
+  upVote = false;
+  downVote = false;
+  userVote = 0;
+
+  vote = new EventEmitter();
+
+  doUpVote() {
+    this.upVote = true;
+    this.downVote = false
+
+    if(this.userVote == 1)
+      return
+
+    this.userVote++;
+    this.vote.emit({ userVote: this.userVote });
+  }
+
+  doDownVote(){
+    this.downVote = true;
+    this.upVote = false;
+
+
+    if (this.userVote == -1)
+      return
+
+    this.userVote--;
+    this.vote.emit({ userVote: this.userVote });
+
+  }
+
+}
